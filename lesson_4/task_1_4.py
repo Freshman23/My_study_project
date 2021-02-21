f0ed4212b5dc86d3133bda6b8a84bb59d8c0b8b1
Freshman23 <inesvezhiy@mail.ru>
2021-02-21 22:52:52 +0300

    task_1_4 modified

--- lesson_4/task_1_4.py
+++ lesson_4/task_1_4.py
@@ -6,8 +6,8 @@ def total_salary_calc(*args):
     wage_rate = int(wage_rate)
     hours_amount = int(hours_amount)
     bonus = int(bonus)
-    print(f'При часовой ставке = {wage_rate}$/ч, часовой выработке = {hours_amount}ч и премии = {bonus}$:\n'
-          f'Зарплата = {wage_rate * hours_amount + bonus}$')
+    print(f'При часовой ставке = {wage_rate}$/ч, часовой выработке = {hours_amount}ч и премии = {bonus}$:')
+    return wage_rate * hours_amount + bonus
 
 
-total_salary_calc(*argv[1:])
+print(f'Заработная плата = {total_salary_calc(*argv[1:])} $')
